154b5028c30d779afd135cc16b2608b6e7ac1162
Angel Gonzales <angel@MacBook-Pro-de-Angel.local>
2021-03-22 23:51:30 +0100

    entregable

--- srcs/influxdb/srcs/start.sh
+++ srcs/influxdb/srcs/start.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
-
-# Start Telegraf and Influxdb.
 /etc/init.d/telegraf start & influxd
+
+influx -execute "CREATE DATABASE telegraf"
+influx -execute "CREATE USER root WITH PASSWORD '123456'"
+influx -execute "GRANT ALL ON telegraf TO root"
--- srcs/mysql/srcs/start.sh
+++ srcs/mysql/srcs/start.sh
@@ -1,19 +1,7 @@
 #! /bin/sh
-
-# Install MariaDB database(mariadb-install-db is a symlink to mysql_install_db).
 mariadb-install-db -u root
-
-# Invoking "mysqld" will start the MySQL server. Terminating "mysqld" will shutdown the MySQL server.
 mysqld -u root & sleep 5
-
-# Create Wordpress database.
 mysql -u root --execute="CREATE DATABASE wordpress;"
-
-# Import previously backed up database to MariaDB database server (wordpress < /wordpress.sql).
 mysql -u root wordpress < wordpress.sql
-
-# Create new user "root" with password "123456" and give permissions.
 mysql -u root --execute="CREATE USER 'root'@'%' IDENTIFIED BY '123456'; GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION; USE wordpress; FLUSH PRIVILEGES;"
-
-# Start Telegraf and sleep infinity for avoid container to stop.
 telegraf & sleep infinite
