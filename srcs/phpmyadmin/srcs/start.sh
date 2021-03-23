f4cee6c7d838a4773a1dd105c9f4d5c3f62d39e8
Angel Gonzales <angel@MacBook-Pro-de-Angel.local>
2021-03-24 00:42:28 +0100

    estable

--- srcs/phpmyadmin/srcs/start.sh
+++ srcs/phpmyadmin/srcs/start.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
 
-# telegraf & php -S 0.0.0.0:5000 -t /www/phpmyadmin/ && nginx -g 'daemon off;'
-# telegraf & php -S 0.0.0.0:5000 -t /www/phpmyadmin/ && nginx
-# nginx -g 'daemon off;'
 php-fpm7  &
 /usr/sbin/nginx -g "daemon off;"
--- srcs/wordpress/srcs/start.sh
+++ srcs/wordpress/srcs/start.sh
@@ -1,8 +1,4 @@
 #!/bin/sh
 
-# Start Telegraf and Wordpress.
-# php --help (-S, --server; Start the internal web server / -t, --docroot; Specify the root of the document for the internal web server).
-# telegraf & php -S 0.0.0.0:5050 -t /var/www/wordpress/ && nginx
-#telegraf
 php-fpm7  &
 /usr/sbin/nginx -g "daemon off;"
